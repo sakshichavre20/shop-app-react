7db7509b87ced63a62dc973f5fe18f9309dff9f9
sakshichavre20 <chavresakshi@gmail.com>
2023-09-18 22:53:39 +0530

    [ADD] cart project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/app.scss";
+import Cart from "./components/Cart";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      <Toaster />
+    </Router>
   );
-}
+};
 
 export default App;
